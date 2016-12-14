b7e73693eb815ac88a9c6ae1b9b952ff5e71fc73
andylushman <andylushman@gmail.com>
2016-12-13 17:13:20 -0700

    correct formula for BMI added

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   var h = $("#height").val();
   var w = $("#weight").val();
     $("#results").click(function(){
-      var sum = h + w;
-      alert(sum);
+      var bmi = w/(h)2;
     });
 });

f30390b9f630d3851f144afd2aae2c9db59f47b2
Chris Fullinwider <cfullinw34@gmail.com>
2016-12-13 23:38:47 -0700

    turning spaces into tabs

--- main.js
+++ main.js
@@ -11,90 +11,90 @@ $(document).ready(function(){
 		$obeseThree : $('.obesity-class-3-outline')
 	};
 
-    function handleCalculateBmi (evt) {
-    	evt.preventDefault();
+	function handleCalculateBmi (evt) {
+		evt.preventDefault();
 
-    	// cache variables and values
-	    var $h = $("#height");
-	    var $w = $("#weight");
-	    var h = $h.val();
-	    var w = $w.val();
+		// cache variables and values
+		var $h = $("#height");
+		var $w = $("#weight");
+		var h = $h.val();
+		var w = $w.val();
 
-	    resetOverlays();
+		resetOverlays();
 
-	    // Check the form fields before doing anything
-	    if (!h){
+		// Check the form fields before doing anything
+		if (!h){
 
-	    	alert("You must have a height!");
-	    	return; // prevent function from continuing to execute if the field is empty
+			alert("You must have a height!");
+			return; // prevent function from continuing to execute if the field is empty
 
-	    } else if (!w) {
+		} else if (!w) {
 
-	    	alert("You must have a width");
-	    	return;
-	    }
+			alert("You must have a width");
+			return;
+		}
 
 
-	    var bmi = w/Math.pow(h, 2);
+		var bmi = w/Math.pow(h, 2);
 
-	    return revealBmi(bmi);
-    }
+		return revealBmi(bmi);
+	}
 
-    function revealBmi(bmi) {
+	function revealBmi(bmi) {
 
-    	if (bmi < 18.5) {
-    		alert('PUT SOME MEAT ON THOSE BONES!!!  YOU\'RE SO SKINNY!!!')
-    	}
+		if (bmi < 18.5) {
+			alert('PUT SOME MEAT ON THOSE BONES!!!  YOU\'RE SO SKINNY!!!')
+		}
 
-    	if (bmi >= 18.5 && bmi < 25) {
+		if (bmi >= 18.5 && bmi < 25) {
 
-    		Overlays.$normal.removeClass('hidden');
-    		return;
-    	}
+			Overlays.$normal.removeClass('hidden');
+			return;
+		}
 
-    	if (bmi >= 25 && bmi < 30) {
+		if (bmi >= 25 && bmi < 30) {
 
-    		Overlays.$over.removeClass('hidden');
-    		return;
-    	}
+			Overlays.$over.removeClass('hidden');
+			return;
+		}
 
-    	if (bmi >= 30 && bmi < 35) {
+		if (bmi >= 30 && bmi < 35) {
 
-    		Overlays.$obeseOne.removeClass('hidden');
-    		return;
-    	};
+			Overlays.$obeseOne.removeClass('hidden');
+			return;
+		};
 
-    	if (bmi >= 35 && bmi <= 40) {
+		if (bmi >= 35 && bmi <= 40) {
 
-    		Overlays.$obeseTwo.removeClass('hidden');
-    		return;
-    	};
+			Overlays.$obeseTwo.removeClass('hidden');
+			return;
+		};
 
-    	if (bmi >= 40 && bmi <= 45) {
+		if (bmi >= 40 && bmi <= 45) {
 
-    		Overlays.$obeseThree.removeClass('hidden');
-    		return;
-    	};
+			Overlays.$obeseThree.removeClass('hidden');
+			return;
+		};
 
-    	if (bmi > 45) {
-    		alert('... I don\'t even believe you...');
-    	}
+		if (bmi > 45) {
+			alert('... I don\'t even believe you...');
+		}
 
-    }
+	}
 
-    function resetOverlays(){
+	function resetOverlays(){
 
-    	for (var key in Overlays){
+		for (var key in Overlays){
 
-    		Overlays[key].addClass('hidden');
-    	}
-    }
+			Overlays[key].addClass('hidden');
+		}
+	}
 
-    $root.on(
-    	'submit',
-    	'.calculator-form',
-    	handleCalculateBmi
-    );
+	$root.on(
+		'submit',
+		'.calculator-form',
+		handleCalculateBmi
+	);
 
 
 });
